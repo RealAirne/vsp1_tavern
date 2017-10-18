685585e351fb06a6252a39182ca5cc3fe7178a5a
airne <arnemt@web.de>
2017-10-18 11:27:15 +0200

    dependencies für spark (rest) eingefügt. Jar not working yet

---
+++ tavern/src/Main.java
@@ -0,0 +1,10 @@
+import static spark.Spark.*;
+
+/**
+ * Created by airne on 18.10.17 for testing around with the new Spark-framework
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Welcome to Summoners Rift");
+    }
+}
--- tavern/src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by airne on 18.10.17.
- */
-public class main {
-    public static void main(String args[]){
-        System.out.println("Welcome to Summoners Rift");
-    }
-}
