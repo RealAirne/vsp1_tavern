0fc3d67f68ba6619e0ff244dd38087ce043153cd
SCArtis <jerom.schult@gmx.de>
2017-11-24 15:25:24 +0100

    bully started

--- app.py
+++ app.py
@@ -10,24 +10,27 @@ HEADER_APPL_JSON = {'content-type': 'application/json; charset=UTF-8'}
 
 # A global variable to manage the hirings
 HIRINGS = []
+GROUP_MEMBERS = [{'name': 'Arne', 'url': 'url'}, {'name': 'Jerom', 'url': 'url'}]
 
 # TODO do a research on how to use URIs (or what's meant here)
-userdata_user = "'user': '/users/Jaume',"
-userdata_idle = "'idle': False,"
-userdata_group = "'group': None,"
-userdata_hirings = "'hirings': '/hirings',"
-userdata_assignments = "'assignments': 'assignments',"
-userdata_messages = "'messages': '/messages'"
+# userdata_user = "'user': '/users/Jaume',"
+# userdata_idle = "'idle': False,"
+# userdata_group = "'group': None,"
+# userdata_hirings = "'hirings': '/hirings',"
+# userdata_assignments = "'assignments': 'assignments',"
+# userdata_messages = "'messages': '/messages'"
+#
+# userdata = {
+# userdata_user + userdata_idle + userdata_group + userdata_hirings + userdata_assignments + userdata_messages}
 
-userdata = {userdata_user + userdata_idle + userdata_group + userdata_hirings + userdata_assignments + userdata_messages}
 
-# userdata = {'user': 'link to the registered user account',
-#             'idle': False,
-#             'group': 'url to the group you are in',
-#             'hirings': 'uri to which one may post to hire you for a group',
-#             'assignments': 'uri to which one may post an assignment',
-#             'messages': 'ri to which one may post messages'
-#             }
+userdata = {'user': 'link to the registered user account',
+            'idle': False,
+            'group': 'url to the group you are in',
+            'hirings': 'uri to which one may post to hire you for a group',
+            'assignments': 'uri to which one may post an assignment',
+            'messages': 'ri to which one may post messages'
+            }
 
 
 # def get_login_token(user, passw):
@@ -39,7 +42,7 @@ userdata = {userdata_user + userdata_idle + userdata_group + userdata_hirings + 
 
 # GET returns
 @app.route('/')
-def hello_world():
+def return_userdata():
     json.dumps(userdata)
     return json.dumps(userdata)
 
@@ -130,6 +133,49 @@ def create_group():
     data = ""
     requests.post(BLACKBOARD_URL, data)
 
+    #####################Bully Algorithm########################
+
+def bully():
+        send_election()
+
+
+def send_election():
+        # throw exception if there are no bigger ones or if nobody is answering
+        for member in GROUP_MEMBERS:
+            if (member['name'] > 'Jaume'):
+                #TODO: Send
+                print member['name']
+
+        #response = requests.post("http://0.0.0.0:80/election", {'payload': 'election'})
+
+@app.route('/election', methods=['POST'])
+def election():
+    if request.data['payload'] == 'election':
+        return request.data
+
+
+#     @app.route('/hirings', methods=['POST'])
+#     def post_hiring():
+#         if request.method == 'POST':
+#             request_data = json.loads(request.data)
+#             try:
+#                 check_hiring_data(request_data)
+#
+#             except KeyError:
+#                 bad_request_response = make_response(
+#                     "The body needs to exactly contain the following keys: group, quest, "
+#                     "message", 400)
+#                 return bad_request_response
+#             # HIRINGS are stored as dicts
+#             list.append(HIRINGS, request_data)
+#             print("actual value of HIRINGS: " + str(HIRINGS))
+#             response = make_response("Hiring posted successfully", 200)
+#             return response
+#         else:
+#             print("There is only a POST allowed here.")
+#             not_allowed_response = make_response(405)
+#             return not_allowed_response
+
 
 def main():
     print("HEJEHEHEHEJEHEJEH")
@@ -138,9 +184,14 @@ def main():
     # DISCOVERED_IP = 'http://' + str(BLACKBOARD_IP) + ':' + str(DISCOVERED_PORT)
     # print(DISCOVERED_IP)
     # register_at_tavern()
+    #bully()
 
 
 main()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
+
+
+
+
